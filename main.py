a431ea7ae1165effbd67a2a01eafd7e1e4a8a465
ShrushtiGirap <77224366+ShrushtiGirap@users.noreply.github.com>
2025-08-22 22:55:45 +0530

    Initial commit - PORTFOLIO WEBSITE

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Serve static files (CSS, images, etc.)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Set up templates
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/project", response_class=HTMLResponse)
+async def projects(request: Request):
+    return templates.TemplateResponse("project.html", {"request": request})
+
+@app.get("/about", response_class=HTMLResponse)
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
